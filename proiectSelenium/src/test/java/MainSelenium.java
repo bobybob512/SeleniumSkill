4e2a389277174de916222b4a587eee26b20bfd32
Ionut Moldovan <i.moldovan@giotto.ai>
2024-01-21 13:07:02 +0200

    maven

---
+++ proiectSelenium/src/test/java/MainSelenium.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainSelenium {
+    public static void main(String[] args) {
+        //System.setProperty("webdriver.chrome.driver", "src/test/chromedriver-win64.zip");
+        ChromeDriver driver = new ChromeDriver();
+    }
+}
