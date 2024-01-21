5d5e9b41a59bf4ca981d9d1967fe5162e4c609a4
Ionut Moldovan <i.moldovan@giotto.ai>
2024-01-21 16:01:31 +0200

    webdrivermanager

--- src/test/java/MainSelenium.java
+++ src/test/java/MainSelenium.java
@@ -1,8 +1,13 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MainSelenium {
     public static void main(String[] args) {
         //System.setProperty("webdriver.chrome.driver", "src/test/chromedriver-win64.zip");
+        //ChromeDriver driver = new ChromeDriver();
+
+        WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
     }
 }
