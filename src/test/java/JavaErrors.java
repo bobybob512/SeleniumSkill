87a38dc22ead017dc49dbb0842e6b6fc8e1a09eb
Bob <>
2024-01-31 11:48:54 +0200

    definitions of throw, throws, try-catch-finally

---
+++ src/test/java/JavaErrors.java
@@ -0,0 +1,13 @@
+public class JavaErrors {
+
+    public static void main(String[] args) {
+
+        //throw
+        //throws
+        //try - catch - finally
+
+        //throw - ne folosim de el ca sa aruncam explicit o eroare
+        //throws - atentionare ca o metoda sau un constructor arunca o eroare/exceptie
+        //try - catch - finally -> handlers
+    }
+}
