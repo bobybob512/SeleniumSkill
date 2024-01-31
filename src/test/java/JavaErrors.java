b7f1e603a6c3871fc6f8e19351e9885eea64db1a
Bob <>
2024-01-31 12:31:33 +0200

    throws_checked exception

--- src/test/java/JavaErrors.java
+++ src/test/java/JavaErrors.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class JavaErrors {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //throw
         //throws
@@ -9,5 +14,23 @@ public class JavaErrors {
         //throw - ne folosim de el ca sa aruncam explicit o eroare
         //throws - atentionare ca o metoda sau un constructor arunca o eroare/exceptie
         //try - catch - finally -> handlers
+
+        checkedException();
+        System.out.println("S-a terminat metoda");
+
+    }
+
+    //checked exceptions
+    public static void checkedException() throws IOException {
+        FileReader file = new FileReader("C:\\Users\\ciurt\\OneDrive\\Desktop\\TestSkillBrain.txt");
+        BufferedReader fileInput = new BufferedReader(file);
+        System.out.println(fileInput.readLine());
+        fileInput.close();
+
+    }
+
+    //unchecked exceptions
+    public static void uncheckedExceptions(){
+
     }
 }
