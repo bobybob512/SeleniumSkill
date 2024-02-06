8586d3b126f28237be8c0594bdec5c3e050afea2
Bob <>
2024-02-06 19:32:53 +0200

    groups

--- src/test/java/com/first/pachet/SeleniumAnnotations.java
+++ src/test/java/com/first/pachet/SeleniumAnnotations.java
@@ -1,15 +1,15 @@
-package com.first.packet;
+package com.first.pachet;
 
 import org.testng.annotations.*;
 
 public class SeleniumAnnotations {
 
-    @BeforeSuite
+    @BeforeSuite(alwaysRun = true)
     public void beforeSuite(){
         System.out.println("beforeSuite");
     }
 
-    @BeforeTest
+    @BeforeTest(alwaysRun = true)
     public void beforeTest(){
         System.out.println("beforeTest");
     }
@@ -19,12 +19,17 @@ public void beforeGroups(){
     System.out.println("beforeGroups");
 }
 
-    @BeforeClass
+    @BeforeClass(alwaysRun = true)
     public void beforeClass(){
         System.out.println("beforeClass");
     }
 
-    @BeforeMethod
+    @BeforeClass(groups = {"Grupul 1"})
+    public void beforeGroupClass(){
+        System.out.println("beforeGroupClass");
+    }
+
+    @BeforeMethod(alwaysRun = true)
     public void beforeMethod(){
         System.out.println("beforeMethod");
     }
@@ -34,17 +39,17 @@ public void beforeGroups(){
         System.out.println("test1");
     }
 
-    @Test
+    @Test(groups = {"Grupul 2"})
     public void test2(){
         System.out.println("test2");
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void afterMethod(){
         System.out.println("afterMethod");
     }
 
-    @AfterClass
+    @AfterClass(alwaysRun = true)
     public void afterClass(){
         System.out.println("afterClass");
     }
@@ -54,12 +59,12 @@ public void beforeGroups(){
         System.out.println("afterGroups");
     }
 
-    @AfterTest
+    @AfterTest(alwaysRun = true)
     public void afterTest(){
         System.out.println("afterTest");
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void afterSuite(){
         System.out.println("afterSuite");
     }
